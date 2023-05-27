a81a325bc1071e3f8f651f3506a187735d24912d
Kryštof Krátký <krystof.kratky2003@gmail.com>
2023-05-27 23:17:57 +0200

    fix host

--- src/templates/asset.ts
+++ src/templates/asset.ts
@@ -2,7 +2,7 @@ import body from "./_body.ts";
 
 const CDN = "https://images.weserv.nl/?q=100&maxage=31d&url=";
 
-export default (images: string[]) =>
+export default (images: string[], host: string) =>
 	body(`
 <div id="carouselIndicators" class="carousel slide bg-dark" data-bs-ride="true">
 	<div class="carousel-indicators">
@@ -25,7 +25,7 @@ export default (images: string[]) =>
 					<div class="carousel-item ` +
 					(index === 0 ? "active" : "") +
 					`">
-						<img class="d-block vh-100 vw-100 object-fit-contain" src="${CDN}${window.location.host}/asset/${image}" alt="img-${index}">
+						<img class="d-block vh-100 vw-100 object-fit-contain" src="${CDN}${host}/asset/${image}" alt="img-${index}">
 					</div>
 					`
 			)
